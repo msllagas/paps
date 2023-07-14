1d80a08ff9fa0d4f9ebdc2438643070514c83ba4
msllagas <mndyllagas@gmail.com>
2023-07-14 18:37:22 +0800

    add scroll event on header

--- app.js
+++ app.js
@@ -20,27 +20,43 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 function openNav() {
-  const sidebar = document.getElementById('mySidenav');
-  const overlay = document.getElementById('overlay');
+  const sidebar = document.getElementById("mySidenav");
+  const overlay = document.getElementById("overlay");
   sidebar.style.width = "250px";
 
-  sidebar.classList.add('show');
-  overlay.classList.add('show');
-  if (typeof window != 'undefined' && window.document) {
-    document.body.style.overflow = 'hidden';
-}
+  sidebar.classList.add("show");
+  overlay.classList.add("show");
+  if (typeof window != "undefined" && window.document) {
+    document.body.style.overflow = "hidden";
+  }
 }
 
 function closeNav() {
-  const sidebar = document.getElementById('mySidenav');
-  const overlay = document.getElementById('overlay');
+  const sidebar = document.getElementById("mySidenav");
+  const overlay = document.getElementById("overlay");
   sidebar.style.width = "0";
 
-  sidebar.classList.remove('show');
-  overlay.classList.remove('show');
-  document.body.style.overflow = 'unset';
+  sidebar.classList.remove("show");
+  overlay.classList.remove("show");
+  document.body.style.overflow = "unset";
 }
 
-  
-  
-  
+var header = document.querySelector(".header");
+var navLink = document.querySelectorAll(".nav__link");
+var headerMenu = document.querySelector(".header__menu");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+    navLink.forEach(function (link) {
+      link.classList.add("scrolled");
+    });
+    headerMenu.classList.add('scrolled')
+  } else {
+    header.classList.remove("scrolled");
+    navLink.forEach(function (link) {
+      link.classList.remove("scrolled");
+    });
+
+    headerMenu.classList.remove('scrolled')
+  }
+});
